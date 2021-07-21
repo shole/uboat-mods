695c4087890a9cea3dc1f09dabbc453c3679862b
Paavo Happonen <pahappo@gmail.com>
2021-07-21 05:49:15 +0300

    updated for B129 compliance

--- Assets/Mods/no-discipline/Source/NoDiscipline.cs
+++ Assets/Mods/no-discipline/Source/NoDiscipline.cs
@@ -1,18 +1,27 @@
+using UBOAT.Game.Core.Serialization;
 using UBOAT.Game.Scene.Entities;
 using UnityEngine;
 
-public class NoDiscipline : MonoBehaviour {
-	private PlayerShip playership;
+namespace UBOAT.Mods.NoDiscipline {
+	[NonSerializedInGameState]
+	public class NoDiscipline : MonoBehaviour {
+		private PlayerShip playership;
 
-	void Update() {
-		if ( playership == null ) {
-			playership = GameObject.FindObjectOfType<PlayerShip>();
-			if ( playership != null ) {
-				Debug.Log("No Discipline Applied");
+		void Start() {
+			Debug.Log("[NoDiscipline] Start..");
+		}
+
+		void Update() {
+			if ( playership == null ) {
+				// Debug.Log("[NoDiscipline] waiting..");
+				playership = GameObject.FindObjectOfType<PlayerShip>();
+				if ( playership != null ) {
+					Debug.Log("[NoDiscipline] Applied..");
+				}
+			} else {
+				// playership.Discipline.Amount = 1f;
+				playership.Discipline.SetAmountQuiet(1f);
 			}
-		} else {
-			// playership.Discipline.Amount = 1f;
-			playership.Discipline.SetAmountQuiet(1f);
 		}
 	}
 }
--- Assets/Mods/no-discipline/Source/NoDisciplineLoader.cs
+++ Assets/Mods/no-discipline/Source/NoDisciplineLoader.cs
@@ -1,9 +1,24 @@
+using System;
 using UnityEngine;
+using UBOAT.Game;
+using UBOAT.Game.Core.Serialization;
 
-public class NoDisciplineLoader : UBOAT.Game.IUserMod {
-	public void OnLoaded() {
-		GameObject go = new GameObject("NoDiscipline");
-		go.AddComponent<NoDiscipline>();
-		GameObject.DontDestroyOnLoad(go);
+namespace UBOAT.Mods.NoDiscipline {
+	[NonSerializedInGameState]
+	public class NoDisciplineLoader : IUserMod {
+		public static readonly string MODNAME = "[NoDiscipline]";
+
+		public void OnLoaded() {
+			try {
+				GameObject go = new GameObject("[NoDiscipline]");
+				go.AddComponent<NoDiscipline>();
+				GameObject.DontDestroyOnLoad(go);
+				
+				Debug.LogFormat("{0} Loaded successfuly!", MODNAME);
+			} catch ( Exception e ) {
+				Debug.LogFormat("{0} Something is broken!", MODNAME);
+				Debug.LogException(e);
+			}
+		}
 	}
 }

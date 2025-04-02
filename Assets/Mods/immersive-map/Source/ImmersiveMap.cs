e8f715a79ee508ae83a81c19af887228d5b51df2
shole <pahappo@gmail.com>
2025-04-02 09:05:22 +0300

    work on "Interior Audio in Map View" mod

--- Assets/Mods/immersive-map/Source/ImmersiveMap.cs
+++ Assets/Mods/immersive-map/Source/ImmersiveMap.cs
@@ -1,161 +1,158 @@
 using DWS.Common.InjectionFramework;
 using System.Collections;
-using System.Reflection;
-using UBOAT.Game.Core.Data;
 using UBOAT.Game.Core.Serialization;
-using UBOAT.Game.Scene;
+using UBOAT.Game.Core.Time;
 using UBOAT.Game.Scene.Audio;
 using UBOAT.Game.Scene.Camera;
+using UBOAT.Game.Scene.Characters;
 using UBOAT.Game.Scene.Effects;
 using UBOAT.Game.Scene.Entities;
 using UBOAT.Game.Scene.Items;
+using UBOAT.Game.Scene.Tasks;
+using UBOAT.Game.UI;
 using UnityEngine;
-using UnityEngine.Audio;
 
 namespace UBOAT.Mods.ImmersiveMap {
 	[NonSerializedInGameState]
 	public class ImmersiveMap : MonoBehaviour {
-		private float             lastUpdate = 0;
-		private PlayerShip        playership;
-		private DeepAudioListener deepAudioListener;
-		private AudioController   audioController;
-		// private Modifier          deltaModifier;
-		// private Modifier          exteriorVolumeModifier;
-		// private Modifier          interiorVolumeModifier;
-		private Modifier interiorUIModifier;
-		private Modifier exteriorUIModifier;
+		private float                     lastUpdate = 0;
+		private PlayerShip                playership;
+		private DeepAudioListener         deepAudioListener;
+		private AudioController           audioController;
+		private TimeCompressionController timeCompressionController;
+		private GameUI                    gameUI;
 
-		// private bool                  applied    = true;
-		private bool inMap = false;
+		private NavigationTable navigationTable;
+		private Vector3         navigationTableOffset;
 
-		void Start() {
-			Debug.Log("[ImmersiveMap] Start..");
-			StartCoroutine(IUpdate());
-		}
-		// void Update() {
-		// 	if ( interiorUIModifier == null && playership != null && audioController != null ) {
-		// 		// deltaModifier = DeepAudioSource.globalDopplerScale.AddDeltaModifier("MapAudioDoppler", false);
-		// 		// exteriorVolumeModifier=audioController.ExteriorVolume.AddDeltaModifier("MapAudioExteriorVolume", false);
-		// 		// interiorVolumeModifier=audioController.InteriorVolume.AddDeltaModifier("MapAudioInternalVolume", false);
-		// 		// Debug.Log("interiorUIModifierField");
-		// 		// FieldInfo interiorUIModifierField = typeof(AudioController).GetField("interiorUIModifier", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance); // private so reflection is required
-		// 		// Debug.Log("interiorUIModifierObject");
-		// 		// object interiorUIModifierObject = interiorUIModifierField.GetValue(audioController);
-		// 		// Debug.Log("interiorUIModifier");
-		// 		// interiorUIModifier = (Modifier)interiorUIModifierObject;
-		// 		interiorUIModifier = audioController.GetPrivateValue<Modifier>("interiorUIModifier");
-		//
-		// 		// Debug.Log("exteriorUIModifierField");
-		// 		// FieldInfo exteriorUIModifierField = typeof(AudioController).GetField("exteriorUIModifier", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance); // private so reflection is required
-		// 		// Debug.Log("exteriorUIModifierObject");
-		// 		// object exteriorUIModifierObject = exteriorUIModifierField.GetValue(audioController);
-		// 		// Debug.Log("exteriorUIModifier");
-		// 		// exteriorUIModifier = (Modifier)exteriorUIModifierObject;
-		// 		exteriorUIModifier = audioController.GetPrivateValue<Modifier>("exteriorUIModifier");
-		//
-		// 		Debug.Log("got");
-		// 	}
-		// }
-
-
-		void EnteredMap() {
-			deepAudioListener.IsBelowWater = playership.Depth > 1;
-			deepAudioListener.IsInsideBoat = true;
-			// deepAudioListener.Compartment  = playership.Interior.CentralCompartment;
-			deepAudioListener.Compartment   = playership.Interior.GetCompartment("Control Room");
-			// deepAudioListener.Compartment   = null;
-			deepAudioListener.SubmergeLevel = 1f;
-			// deepAudioListener.SetPrivateValue("interiorLevel", 1f);
-
-
-			MainCamera.Instance.CustomAudioMixerControl = true;
-
-			// Debug.Log("interior: "+audioController.InteriorVolume.Value);
-			// Debug.Log("exterior: "+audioController.ExteriorVolume.Value);
-			audioController.SetFullscreenInterfaceMode(false);
-
-			// audioController.AudioMixer.TransitionToSnapshots(new AudioMixerSnapshot[] {
-			// 	audioController.AudioMixer.FindSnapshot("Interior"),
-			// 	audioController.AudioMixer.FindSnapshot("Exterior")
-			// }, new float[] { 0.5f, 0.5f }, 0);
-			audioController.AudioMixer.FindSnapshot("Exterior").TransitionTo(0);
-
-			// MainCamera.Instance.loadingUIController.FinalAudioMixerSnapshot = this.snapshots[this.isInsideBoat ? 0 : 1][0];
-
-			MainCamera.Instance.IsAudioListener      = false;
-			// MainCamera.Instance.ForceInsideBoatState = true;
-
-			// if ( interiorUIModifier != null ) {
-			// 	Debug.Log("in map");
-			// 	Debug.Log("interiorUIModifier.Value " + interiorUIModifier.Value);
-			// 	interiorUIModifier.Value = 100f;
-			// 	Debug.Log("InteriorVolume.Value " + audioController.InteriorVolume.Value);
-			// 	Debug.Log("exteriorUIModifier.Value " + exteriorUIModifier.Value);
-			// 	exteriorUIModifier.Value = 100f;
-			// 	Debug.Log("ExteriorVolume.Value " + audioController.ExteriorVolume.Value);
-			//
-			// 	// audioController.AudioMixer.SetFloat("Interior.Volume", (float)10);
-			// 	// audioController.AudioMixer.SetFloat("Exterior.Volume", (float)10);
-			// 	float interiorv;
-			// 	audioController.AudioMixer.GetFloat("Interior.Volume",out interiorv);
-			// 	Debug.Log("Interior.Volume "+interiorv);
-			// 	float exteriorv;
-			// 	audioController.AudioMixer.GetFloat("Exterior.Volume", out exteriorv);
-			// 	Debug.Log("Exterior.Volume "+exteriorv);
-			// 	// audioController.ExteriorMixerGroup.audioMixer.outputAudioMixerGroup
-			// } else {
-			// 	Debug.Log("modifiers do NOT exist");
-			// }
-
-			// MainCamera.Instance.ResetDopplerEffect();
-			// MainCamera.Instance.MapController.
-			foreach ( PropellerEffects propellerEffectse in FindObjectsOfType<PropellerEffects>(true) ) {
-				propellerEffectse.enabled = true;
+		private bool inMapLastFrame = false; // were we on map last frame
+		private bool inMapNow {
+			get {
+				return
+					gameUI.CurrentMode == GameUI.Mode.Map; // on map screen
+				// MainCamera.Instance.CurrentMode == CameraMode.Map; // maincamera mode is unaware of periscope and other fullscreen UI modes
+				// &&
+				// (
+				// 	!timeCompressionController.TimeCompression // when no time compression
+				// 	||
+				// 	timeCompressionController.TimeCompressionScale.Value < 1000f // or if compression below this
+				// );
 			}
 		}
-		void InMapUpdate() {
-			// deepAudioListener.transform.position = playership.Interior.VirtualScene.SceneToMappedPosition(playership.Interior.transform.position);
-			// deepAudioListener.transform.position = playership.Interior.VirtualScene.GetScenePosition(playership.Interior.transform.position, playership.Interior.VirtualScene);
 
-			// deepAudioListener.transform.position = playership.NavigationTable.transform.position;
-			// deepAudioListener.transform.position = playership.Interior.CentralCompartment.transform.position;
-			// deepAudioListener.transform.position = playership.transform.position;
-			deepAudioListener.transform.localPosition = new Vector3(0, .75f, 0);
-			deepAudioListener.transform.localRotation = Quaternion.identity;
-			// deepAudioListener.transform.rotation = playership.transform.rotation;
-		}
-		void ExitedMap() {
+		private Coroutine updateCoroutine;
 
+		void Update() {
+			if ( updateCoroutine == null || lastUpdate - Time.realtimeSinceStartup > 10f ) { // if coroutine timed out or missing, respawn
+				if ( updateCoroutine != null ) {
+					StopCoroutine(updateCoroutine);
+				}
+				Debug.Log("[ImmersiveMap] Start..");
+				updateCoroutine = StartCoroutine(IUpdate());
+			}
 		}
 
 		IEnumerator IUpdate() {
 			while ( true ) {
+				lastUpdate = Time.realtimeSinceStartup;
 				yield return null;
-				if ( playership == null || deepAudioListener == null || audioController == null ) { // wait for ingame state
-					yield return new WaitForSeconds(1f);
+				if ( playership == null || deepAudioListener == null || audioController == null || timeCompressionController == null || gameUI == null || navigationTable == null ) { // wait for ingame state
+					yield return new WaitForSecondsRealtime(1f);
+
+					Debug.Log("[ImmersiveMap]  ref");
+
 					playership        = FindObjectOfType<PlayerShip>();
 					deepAudioListener = FindObjectOfType<DeepAudioListener>();
+					gameUI            = FindObjectOfType<GameUI>();
 					audioController   = ScriptableObjectSingleton.LoadSingleton<AudioController>();
-					// audioController   = Globals.Instance.GetPrivateValue<AudioController>("audioController");
-					// audioController   = FindObjectOfType<AudioController>();
-					// Debug.Log("playership: " + playership);
-					// Debug.Log("deepAudioListener: " + deepAudioListener);
-					// Debug.Log("audioController: " + audioController);
-					Debug.Log("got references");
-					yield return null;
+					BackgroundTasksManager backgroundTasksManager = ScriptableObjectSingleton.LoadSingleton<BackgroundTasksManager>();
+					timeCompressionController = backgroundTasksManager?.GetRunningTask<TimeCompressionController>();
+
+					navigationTable = FindObjectOfType<NavigationTable>(true);
+
+					// MainCamera.Instance.ModeChanged+=modeChanged(); // possible future improvement?
+
+					// Debug.Log("ref playership: " + playership);
+					// Debug.Log("ref deepAudioListener: " + deepAudioListener);
+					// Debug.Log("ref gameUI: " + gameUI);
+					// Debug.Log("ref audioController: " + audioController);
+					// Debug.Log("ref timeCompressionController: " + timeCompressionController);
+					// Debug.Log("ref navigationTable: " + navigationTable);
+
 					yield return null;
 				} else {
-					if ( MainCamera.Instance.CurrentMode == CameraMode.Map && !inMap ) {
-						inMap = true;
+					if ( inMapNow && !inMapLastFrame ) {
+						yield return null; // let map init happen
 						EnteredMap();
-					} else if ( MainCamera.Instance.CurrentMode == CameraMode.Map && inMap ) {
+					} else if ( inMapNow && inMapLastFrame ) {
 						InMapUpdate();
-					} else if ( MainCamera.Instance.CurrentMode != CameraMode.Map && inMap ) {
-						inMap = false;
+					} else if ( !inMapNow && inMapLastFrame ) {
 						ExitedMap();
 					}
+
+					inMapLastFrame = inMapNow;
 				}
 			}
 		}
+
+		void EnteredMap() {
+			deepAudioListener.IsBelowWater = playership.Depth > 1;
+			deepAudioListener.IsInsideBoat = true;
+			deepAudioListener.Compartment  = playership.Interior.CentralCompartment;
+			// deepAudioListener.Compartment = playership.Interior.GetCompartment("Control Room");
+			// deepAudioListener.Compartment   = null;
+			deepAudioListener.SubmergeLevel = 1f;
+			// deepAudioListener.SetPrivateValue("interiorLevel", 1f);
+
+			MainCamera.Instance.IsAudioListener      = false;
+			MainCamera.Instance.ForceInsideBoatState = true;
+			// MainCamera.Instance.ResetDopplerEffect();
+
+			// audioController.SetFullscreenInterfaceMode(false);
+
+			// MainCamera.Instance.CustomAudioMixerControl = true;
+
+			// audioController.AudioMixer.TransitionToSnapshots(new AudioMixerSnapshot[] {
+			// 	audioController.AudioMixer.FindSnapshot("Interior"),
+			// 	audioController.AudioMixer.FindSnapshot("Exterior")
+			// }, new float[] { 0.1f, 0.1f }, 0);
+			// audioController.AudioMixer.FindSnapshot("Exterior").TransitionTo(0);
+
+			// MainCamera.Instance.loadingUIController.FinalAudioMixerSnapshot = this.snapshots[this.isInsideBoat ? 0 : 1][0];
+
+			// Debug.Log("EnteredMap - PropellerEffects");
+			foreach ( PropellerEffects propellerEffect in FindObjectsOfType<PropellerEffects>(true) ) {
+				// Debug.Log("propeller audiosource: " + propellerEffect.name);
+				propellerEffect.enabled = true;
+				// if ( propellerEffect.GetComponent<AudioSource>() ) {
+				// 	Debug.Log("propeller audiosource: " + propellerEffect.GetComponent<AudioSource>()?.gameObject.name);
+				// 	propellerEffect.GetComponent<AudioSource>().dopplerLevel = 1;
+				// }
+			}
+			// Debug.Log("EnteredMap - done");
+		}
+		void InMapUpdate() {
+			// deepAudioListener.transform.localPosition = new Vector3(0, .75f, 0); // center of compartment
+			deepAudioListener.transform.localPosition = new Vector3(0, 0.6f, navigationTable.transform.position.z);
+
+			deepAudioListener.transform.localRotation = Quaternion.identity; // facing forward
+			// deepAudioListener.transform.localRotation = Quaternion.Euler(0, navigationTable.transform.localPosition.x > 0 ? 90 : -90, 0); // facing map (sideways)
+		}
+		void ExitedMap() {
+			deepAudioListener.IsBelowWater  = false;
+			deepAudioListener.IsInsideBoat  = false;
+			deepAudioListener.Compartment   = null;
+			deepAudioListener.SubmergeLevel = 0f;
+
+			MainCamera.Instance.IsAudioListener      = true;
+			MainCamera.Instance.ForceInsideBoatState = false;
+
+			// Debug.Log("ExitedMap - propellers");
+			// foreach ( PropellerEffects propellerEffectse in FindObjectsOfType<PropellerEffects>(true) ) {
+			// 	propellerEffectse.enabled = false;
+			// }
+			// Debug.Log("ExitedMap - done");
+		}
+
 	}
 }
